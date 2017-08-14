9b49926c036d7e3f13a7b28a4d5d460e1e13041a
mer <iwangyang@qq.com>
2017-08-14 10:57:06 +0800

    pages

--- page/info/e.js
+++ page/info/e.js
@@ -17,40 +17,7 @@ let time = function(z = new Date()) {
 Page({
     // 数据
     data: {
-        // 日期
-        location: '正在定位…',
-        date: {
-            text: time(),
-            time: String( Date.now() ),
-        },
-        choose: [
-            {
-                name: '医疗急救',
-                arr: ['出血', '骨折', '有人受伤', '被压', '无法行动', '意识不清', '生命危机'],
-            },
-            {
-                name: '物资',
-                arr: ['饮用水', '食物', '毛毯', '帐篷'],
-            },
-        ],
-        tip: 0,
-        info: [
-            {
-                name: '个人姓名',
-                key: 'name',
-                val: '李梅梅',
-            },
-            {
-                name: '手机号码',
-                key: 'phone',
-                val: '18494390349',
-            },
-            {
-                name: '身份证号',
-                key: 'idcard',
-                val: '511025199001012345',
-            },
-        ],
+
     },
     // 刷新
     onPullDownRefresh() {
@@ -64,42 +31,6 @@ Page({
         //
     },
     onLoad() {
-        // 登陆
-        wx.BaaS.login().then((res) => {
-            let uid = wx.BaaS.storage.get('uid')
-            wx.BaaS.getUserInfo({
-                userID: uid
-            }).then(res => {
-                // log('用户数据:', res)
-            })
-        }, (err) => {
-            log(err)
-        })
-    },
-    formSubmit(e) {
-        let o = e.detail.value
-        o.date = this.data.date.time
-        log(o)
-        // wx.showModal({
-        //     title: "确认发布吗？",
-        //     content: "需勾选的责任说明",
-        //     cancelColor: "#9B9B9B",
-        //     confirmColor: "#FF633D",
-        //     success: function(res) {
-        //         if (res.confirm) {
-        //             log('确认')
-        //         } else if (res.cancel) {
-        //             log('取消')
-        //         }
-        //     }
-        // })
-    },
-    binType(e) {
-        this.setData({
-            tip: Number(e.currentTarget.dataset.id)
-        })
-    },
-    bindTip(e) {
-        log(e)
+
     },
 })
